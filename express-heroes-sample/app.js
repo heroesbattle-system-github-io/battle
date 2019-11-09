f5ed63ea4550c15d4b2b5e4a47d363563cbb033b
RomanBorr <starmordar@gmail.com>
2019-11-09 13:58:49 +0300

    add room connection

--- express-heroes-sample/app.js
+++ express-heroes-sample/app.js
@@ -1,35 +1,62 @@
 let WebSocketServer = new require('ws');
 
-let gameRooms = [];
+const ZERO_IN_ROOM = 0;
+const ONE_IN_ROOM = 1;
+const TWO_IN_ROOM = 2;
+
+let gameRooms = [ZERO_IN_ROOM];
 let clients = {};
 
 var webSocketServer = new WebSocketServer.Server({
-  port: 8081
+  port: 4451
 });
 
-webSocketServer.on('connection', function(ws) {
-  
+webSocketServer.on('connection', function (ws) {
+  let roomId = 1,
+    allBusyRooms = true,
+    startNewGame = false;
+
   for (let i = 0; i < gameRooms.length; i++) {
-    
+
+    if (gameRooms[i] === ONE_IN_ROOM) {
+      roomId = i;
+      gameRooms[i]++;
+      allBusyRooms = false;
+      startNewGame = true;
+
+    } else if (gameRooms[i] !== TWO_IN_ROOM) {
+      roomId = i;
+      gameRooms[i]++;
+      allBusyRooms = false
+    }
   }
 
-  var id = Math.random();
-  clients[id] = ws;
-  console.log("новое соединение " + id);
+  if (allBusyRooms) {
+    gameRooms.push(ONE_IN_ROOM);
+    roomId = gameRooms.length - 1
+  }
 
-  ws.on('message', function(message) {
-    console.log('получено сообщение ' + message);
-
-    for (var key in clients) {
-      clients[key].send(message);
+  const clientId = Object.keys(clients).length + 1;
+  clients[clientId] = ws;
+  clients[clientId].send(`{"roomID":"${roomId}"}`)
+ 
+  if (startNewGame) {
+    for (const key in clients) {
+      clients[key].send(`{"startGame":"${true}", "roomID":"${roomId}"}`);
     }
+  }
 
-    ws.send("data");
-  });
+  // ws.on('message', function (message) {
+  //   console.log('receive message ' + message);
 
-  ws.on('close', function() {
-    console.log('соединение закрыто ' + id);
-    delete clients[id];
+  //   for (var key in clients) {
+  //     clients[key].send(message);
+  //   }
+  // });
+
+  ws.on('close', function () {
+    console.log('closing connection ' + clientId);
+    delete clients[clientId];
   });
 
 });
--- frontend/js/entry.js
+++ frontend/js/entry.js
@@ -1,12 +1,19 @@
-const socket = new WebSocket("ws://localhost:8081");
+const socket = new WebSocket("ws://localhost:4451");
+
+let gameID = 0;
 
 socket.onmessage = function (event) {
     const incomingMessage = event.data;
+    let jsonData = JSON.parse(incomingMessage);
+    console.log(jsonData)
+    if (jsonData["roomID"] !== undefined) gameID = jsonData["roomID"];
 
-    let dataJSON = JSON.parse(incomingMessage);
-
-    console.log(incomingMessage, dataJSON)
+    if (jsonData["startGame"] !== undefined) {
+        if (jsonData["roomID"] == gameID) {
+            window.location.replace("file:///home/roman/battle/frontend/views/battle.html")
+        }
+    }
 };
 
-socket.send('{ "playerId":"32", "yourTurn":"false"}');
+// socket.send('{ "playerId":"32", "yourTurn":"false"}');
 
