bbb6913a83dd84434e423026be91252c0a0a3bfd
RomanBorr <starmordar@gmail.com>
2019-11-09 19:25:07 +0300

    add structure of project

--- express-heroes-sample/app.js
+++ express-heroes-sample/app.js
@@ -4,21 +4,33 @@ const { Client } = require("./classes/Client.class");
 const { GameRoom } = require("./classes/GameRoom.class");
 
 let gameRooms = [new GameRoom(1, null, null)];
+let numberOfClients = 1;
 let clients = [];
 
 var webSocketServer = new WebSocketServer.Server({
   port: 4451
 });
 
+function someServerCalculationAboutUnit(gameRoom) {
+  //some stuff
+
+  gameRoom.firstClient.ctx.send(`{"unitNumber":1}`);
+  gameRoom.secondClient.ctx.send(`{"unitNumber":1}`);
+}
+
 function sendStartGame(gameRoom) {
   gameRoom.firstClient.ctx.send(`{"roomID":"${gameRoom.id}"}`);
-  gameRoom.secondClient.ctx.send(`{"roomID":"${gameRoom.id}"}`)
+  gameRoom.secondClient.ctx.send(`{"roomID":"${gameRoom.id}"}`);
+
+  gameRoom.firstClient.ctx.send(`{"yourTurn":true}`);
+  gameRoom.secondClient.ctx.send(`{"yourTurn":false}`);
+
+  someServerCalculationAboutUnit(gameRoom);
 }
 
 webSocketServer.on('connection', function (ws) {
-  let numberOfClients = clients.length,
-    newClientId = numberOfClients + 1,
-    client = new Client(ws, newClientId);
+  let clientID = numberOfClients++;
+  let client = new Client(ws, clientID);
   clients.push(client);
 
   let numberOfGameRooms = gameRooms.length,
@@ -30,16 +42,19 @@ webSocketServer.on('connection', function (ws) {
     if (gameRooms[i].firstClient === null && gameRooms[i].secondClient === null) {
       gameRooms[i].firstClient = client;
       allRoomsBusy = false;
+      break;
     }
     else if (gameRooms[i].firstClient === null && gameRooms[i].secondClient !== null) {
       gameRooms[i].firstClient = client;
       allRoomsBusy = false;
       sendStartGame(gameRooms[i]);
+      break;
     }
     else if (gameRooms[i].firstClient !== null && gameRooms[i].secondClient === null) {
       gameRooms[i].secondClient = client;
       allRoomsBusy = false;
       sendStartGame(gameRooms[i]);
+      break;
     }
   }
   if (allRoomsBusy) {
@@ -47,25 +62,27 @@ webSocketServer.on('connection', function (ws) {
     gameRooms[gameRooms.length - 1].firstClient = client;
   }
 
-  // ws.on('message', function (message) {
-  //   console.log('receive message ' + message);
-
-  //   for (var key in clients) {
-  //     clients[key].send(message);
-  //   }
-  // });
+  ws.on('message', function (message) {
+   console.log(message)
+  });
 
   ws.on('close', function () {
-    
     for (let i = 0; i < clients.length; i++) {
-      if (clients[i]["id"] === newClientId) { clients.splice(i, 1); }
+      if (clients[i]["id"] === clientID) {
+        clients.splice(i, 1);
+      }
     }
 
     for (let i = 0; i < gameRooms.length; i++) {
-      console.log(gameRooms[i]["firstClient"].id, newClientId)
-      if (gameRooms[i]["firstClient"].id === newClientId) {
+
+      if (gameRooms[i]["firstClient"] !== null &&
+        gameRooms[i]["firstClient"].id === clientID) {
+
         gameRooms[i]["firstClient"] = null;
-      } else if (gameRooms[i]["secondClient"].id === newClientId) {
+      }
+      else if (gameRooms[i]["secondClient"] !== null &&
+        gameRooms[i]["secondClient"].id === clientID) {
+
         gameRooms[i]["secondClient"] = null;
       }
     }
--- frontend/js/entry.js
+++ frontend/js/entry.js
@@ -1,15 +1,104 @@
-const socket = new WebSocket("ws://localhost:4451");
+const socket = new WebSocket("ws://localhost:4451/battle");
 
-let gameID = 0;
+let gameID = undefined,
+    yourTurn = false,
+    activeUnit = null;
 
 socket.onmessage = function (event) {
     const incomingMessage = event.data;
     let jsonData = JSON.parse(incomingMessage);
-    console.log(incomingMessage)
+
     if (jsonData["roomID"] !== undefined) {
         gameID = jsonData["roomID"];
-        // window.location.href = "file:///home/roman/battle/frontend/views/battle.html";
+        startGame();
+    }
+
+    if (jsonData["yourTurn"] !== undefined) {
+        yourTurn = jsonData["yourTurn"];
+    }
+
+    if (jsonData["unitNumber"] !== undefined) {
+        activeUnit = jsonData["unitNumber"];
+        startAttackProcesses(activeUnit, yourTurn);
     }
 };
 
+let images = document.querySelectorAll("#image");
+
+images.forEach(image => {
+    image.addEventListener("mouseover", (ev) => {
+        if (yourTurn === false) return;
+        image.classList.add("on-hover-image");
+    })
+
+    image.addEventListener("mouseleave", (ev) => {
+        if (yourTurn === false) return;
+        image.classList.remove("on-hover-image");
+    })
+
+    image.addEventListener("click", (ev) => {
+        if (yourTurn === false) return;
+        nextTurn();
+    })
+});
+
+function nextTurn() {
+    socket.send(`{"gameID": ${gameID}, "message":"next turn"}`)
+}
+
+function startAttackProcesses(activeUnit, yourTurn) {
+    // if (yourTurn === false) return;
+
+    let activeUnitImg = document.querySelector(`.unit-${activeUnit}`);
+    console.log(activeUnit)
+    activeUnitImg.classList.add("active-unit")
+}
+
+function startGame() {
+    let overBack = document.querySelector(".overflow-company");
+    overBack.classList.add("fadeOut")
+}
+
+function initUnitPositions(image, elementPositionedBy) {
+    let offset = elementPositionedBy.getBoundingClientRect();
+    let centerX = offset.top + offset.width / 2;
+    let centerY = offset.left + offset.height / 2;
+
+    image.style.left = centerY - 50 + 'px';
+    image.style.top = centerX - 100 + 'px';
+}
+
+function initArmy() {
+    let imageMap = [
+        document.querySelector(".img-1"),
+        document.querySelector(".img-2"),
+        document.querySelector(".img-3"),
+        document.querySelector(".img-4"),
+        document.querySelector(".img-5"),
+
+        document.querySelector(".img-1-2"),
+        document.querySelector(".img-2-2"),
+        document.querySelector(".img-3-2"),
+        document.querySelector(".img-4-2"),
+        document.querySelector(".img-5-2")
+    ]
+    let positionMap = [
+        document.querySelector('g[transform="translate(-450,-952)"]'),
+        document.querySelector('g[transform="translate(-600,-693)"]'),
+        document.querySelector('g[transform="translate(-750,-433)"]'),
+        document.querySelector('g[transform="translate(-900, -173)"]'),
+        document.querySelector('g[transform="translate(-1050, 86)"]'),
+
+        document.querySelector('g[transform="translate(1350, 88)"]'),
+        document.querySelector('g[transform="translate(1200, 347)"]'),
+        document.querySelector('g[transform="translate(1050, 606)"]'),
+        document.querySelector('g[transform="translate(900, 865)"]'),
+        document.querySelector('g[transform="translate(750, 1125)"]')
+    ]
+    for (let i = 0; i < imageMap.length; i++) {
+        initUnitPositions(imageMap[i], positionMap[i])
+    }
+}
+
+initArmy();
 
