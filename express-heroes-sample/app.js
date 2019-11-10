9e625b92ad57ba0a177faeca953a0fa728a39dd5
RomanBorr <starmordar@gmail.com>
2019-11-10 13:53:28 +0300

    cleanup code

--- express-heroes-sample/app.js
+++ express-heroes-sample/app.js
@@ -1,100 +1,63 @@
-let WebSocketServer = new require('ws');
+const WebSocket = require('ws');
 
 const { Client } = require("./classes/Client.class");
 const { GameRoom } = require("./classes/GameRoom.class");
 const { _helper } = require("./websocker.helper")
- 
-let gameRooms = [new GameRoom(1, null, null, "")];
-let numberOfClients = 1;
-let clients = [];
 
-var webSocketServer = new WebSocketServer.Server({
+let gameRooms = [new GameRoom(1, null, null, "")];
+let clients = [];
+let numberOfClients = 1;
+
+const webSocketServer = new WebSocket.Server({
   port: 4451
-}); 
+});
 
 webSocketServer.on('connection', function (ws) {
-  let clientID = numberOfClients++;
-  let client = new Client(ws, clientID);
-  clients.push(client);
-
-  let numberOfGameRooms = gameRooms.length,
+  let clientID = numberOfClients++,
+    client = new Client(ws, clientID),
+    numberOfGameRooms = gameRooms.length,
     newGameRoomId = numberOfGameRooms + 1;
 
-  let allRoomsBusy = true;
-     
-  for (let i = 0; i < gameRooms.length; i++) {
-    if (gameRooms[i].firstClient === null && gameRooms[i].secondClient === null) {
-      gameRooms[i].firstClient = client;
-      allRoomsBusy = false;
-      break;
-    }
-    else if (gameRooms[i].firstClient === null && gameRooms[i].secondClient !== null) {
-      gameRooms[i].firstClient = client;
-      allRoomsBusy = false;
-      _helper.startGame(gameRooms[i]);
-      break;
-    }
-    else if (gameRooms[i].firstClient !== null && gameRooms[i].secondClient === null) {
-      gameRooms[i].secondClient = client;
-      allRoomsBusy = false;
-      _helper.startGame(gameRooms[i]);
-      break;
-    }
-  }
+  clients.push(client);
 
-  if (allRoomsBusy) {
-    gameRooms.push(new GameRoom(newGameRoomId, null, null, ""));
-    gameRooms[gameRooms.length - 1].firstClient = client;
-  }
+  gameRooms = _helper.putPlayerInGameRoom(gameRooms, client, newGameRoomId);
 
   ws.on('message', function (message) {
     let msgData = JSON.parse(message)
-         
-    if (msgData.message === "End Turn") {
+
+    if (msgData.message === _helper.END_TURN_MESSAGE) {
       for (let i = 0; i < gameRooms.length; i++) {
+
         if (gameRooms[i].id === msgData.gameID) {
-          if (gameRooms[i].turn === "firstPlayer") gameRooms[i].turn = "secondPlayer"
-          else if (gameRooms[i].turn === "secondPlayer") gameRooms[i].turn = "firstPlayer"
-          
+          if (gameRooms[i].turn === "firstPlayer")
+            gameRooms[i].turn = "secondPlayer"
+          else if (gameRooms[i].turn === "secondPlayer")
+            gameRooms[i].turn = "firstPlayer"
+
           _helper.updateTurnStatus(gameRooms[i]);
           _helper.setUnitMovingOrder(gameRooms[i]);
         }
+
       }
-    } 
-        
-    if(msgData.message === "attackMonster"){
+    }
+
+    if (msgData.message === _helper.ATTACK_MONSTER_MESSAGE) {
       for (let i = 0; i < gameRooms.length; i++) {
+
         if (gameRooms[i].id === msgData.gameID) {
           let type = msgData.type,
             attackerId = msgData.attacker,
             attackTargetId = msgData.attackTarget;
 
-            _helper.processAttackEvent(type, attackerId, attackTargetId, gameRooms[i])
+          _helper.processAttackEvent(type, attackerId, attackTargetId, gameRooms[i])
         }
-      }
-    }
-  });  
-       
-  ws.on('close', function () {
-    for (let i = 0; i < clients.length; i++) {
-      if (clients[i]["id"] === clientID) {
-        clients.splice(i, 1);
-      }
-    }
-
-    for (let i = 0; i < gameRooms.length; i++) {
-
-      if (gameRooms[i]["firstClient"] !== null &&
-        gameRooms[i]["firstClient"].id === clientID) {
-
-        gameRooms[i]["firstClient"] = null;
-      }
-      else if (gameRooms[i]["secondClient"] !== null &&
-        gameRooms[i]["secondClient"].id === clientID) {
-
-        gameRooms[i]["secondClient"] = null;
+        
       }
     }
   });
 
+  ws.on('close', function () {
+    clients = _helper.removeClientOnCloseEv(clients, clientID)
+    gameRooms = _helper.removeClientFromGameRoom(gameRooms, clientID)
+  });
 });
--- express-heroes-sample/websocker.helper.js
+++ express-heroes-sample/websocker.helper.js
@@ -8,6 +8,40 @@ const _helper = {
     PLAYER_FIRST_TYPE_JSON: '{"type":"first"}',
     PLAYER_SECOND_TYPE_JSON: '{"type":"second"}',
 
+    END_TURN_MESSAGE: "End Turn",
+    ATTACK_MONSTER_MESSAGE: "attackMonster",
+
+    putPlayerInGameRoom(gameRooms, client, newGameRoomId) {
+        let allRoomsBusy = true;
+
+        for (let i = 0; i < gameRooms.length; i++) {
+            if (gameRooms[i].firstClient === null && gameRooms[i].secondClient === null) {
+                gameRooms[i].firstClient = client;
+                allRoomsBusy = false;
+                break;
+            }
+            else if (gameRooms[i].firstClient === null && gameRooms[i].secondClient !== null) {
+                gameRooms[i].firstClient = client;
+                allRoomsBusy = false;
+                this.startGame(gameRooms[i]);
+                break;
+            }
+            else if (gameRooms[i].firstClient !== null && gameRooms[i].secondClient === null) {
+                gameRooms[i].secondClient = client;
+                allRoomsBusy = false;
+                this.startGame(gameRooms[i]);
+                break;
+            }
+        }
+
+        if (allRoomsBusy) {
+            gameRooms.push(new GameRoom(newGameRoomId, null, null, ""));
+            gameRooms[gameRooms.length - 1].firstClient = client;
+        }
+
+        return gameRooms;
+    },
+
     updateTurnStatus(gameRoom) {
         if (gameRoom.turn === this.FIRST_PLAYER_TURN) {
             gameRoom.firstClient.ctx.send(this.YOUR_TURN_TRUE_JSON);
@@ -58,6 +92,34 @@ const _helper = {
             "attackTarget":${attackTargetId},
             "typeAttacker":"${type}"
         }`);
+    },
+
+    removeClientOnCloseEv(clients, clientID) {
+        for (let i = 0; i < clients.length; i++) {
+            if (clients[i]["id"] === clientID) {
+                clients.splice(i, 1);
+            }
+        }
+
+        return clients
+    },
+
+    removeClientFromGameRoom(gameRooms, clientID) {
+        for (let i = 0; i < gameRooms.length; i++) {
+
+            if (gameRooms[i]["firstClient"] !== null &&
+                gameRooms[i]["firstClient"].id === clientID) {
+
+                gameRooms[i]["firstClient"] = null;
+            }
+            else if (gameRooms[i]["secondClient"] !== null &&
+                gameRooms[i]["secondClient"].id === clientID) {
+
+                gameRooms[i]["secondClient"] = null;
+            }
+        }
+
+        return gameRooms
     }
 }
 
