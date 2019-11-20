4a90f93cd168b355053f83817d00fee5a5359d9c
RomanBorr <starmordar@gmail.com>
2019-11-20 11:46:13 +0300

    add unit animation

--- frontend/hexGrid/unit.canvas.js
+++ frontend/hexGrid/unit.canvas.js
@@ -1,10 +1,11 @@
-const canvas = document.getElementById("units");
-const ctx = canvas.getContext('2d');
+const canvasUnits = document.getElementById("units");
+const ctx = canvasUnits.getContext('2d');
 
 function setUnitsOnInitialPositions(units) {
     for (let i = 0, p = Promise.resolve(); i < units.length; i++) {
         p = p.then(_ => new Promise(resolve => {
             let img = new Image();
+            img.src = units[i].src;
 
             img.onload = function () {
                 ctx.drawImage(
@@ -13,12 +14,56 @@ function setUnitsOnInitialPositions(units) {
                     units[i].size.width, units[i].size.height
                 )
 
-                resolve()
+                resolve(true)
             }
-
-            img.src = units[i].src;
         }));
     }
 }
 
-setUnitsOnInitialPositions(_unitHelper.INIT_UNITS_IMG);
+setUnitsOnInitialPositions(_unitHelper.INIT_UNITS_IMG, 1);
+
+let unitFirstSrc = new Image();
+unitFirstSrc.src = _unitHelper.INIT_UNITS_IMG[1].src;
+
+let unitSecondSrc = new Image();
+unitSecondSrc.src = _unitHelper.INIT_UNITS_IMG[6].src;
+
+let x = 0, y = 0;
+
+function animate(unitID) {
+    let id = window.requestAnimationFrame(() => {
+        animate(unitID);
+    });
+
+    ctx.clearRect(0, 0, canvasUnits.width, canvasUnits.height);
+
+    for (let i = 0; i < _unitHelper.INIT_UNITS_IMG.length; i++) {
+        if (i === unitID) continue;
+
+        let src = unitSecondSrc
+        if (i <= 5) src = unitFirstSrc
+
+        ctx.drawImage(
+            src,
+            _unitHelper.INIT_UNITS_IMG[i].position.x,
+            _unitHelper.INIT_UNITS_IMG[i].position.y,
+            _unitHelper.INIT_UNITS_IMG[i].size.width,
+            _unitHelper.INIT_UNITS_IMG[i].size.height
+        )
+    }
+
+    let src = unitSecondSrc
+    if (unitID <= 5) src = unitFirstSrc
+    ctx.drawImage(
+        src,
+        _unitHelper.INIT_UNITS_IMG[unitID].position.x + x,
+        _unitHelper.INIT_UNITS_IMG[unitID].position.y + y,
+        _unitHelper.INIT_UNITS_IMG[unitID].size.width,
+        _unitHelper.INIT_UNITS_IMG[unitID].size.height
+    )
+    x++;
+    y++;
+    if (x >= 100) cancelAnimationFrame(id)
+}
+
+animate(1);
